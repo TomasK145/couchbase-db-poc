448fa91d9989b2ea78b56ee9d2b505517f94aa8e
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-11-11 14:33:17 +0100

    https://blog.couchbase.com/asp-net-core-microservices-getting-started/

--- HelloMicroservice/Controllers/HomeController.cs
+++ HelloMicroservice/Controllers/HomeController.cs
@@ -6,21 +6,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HelloMicroservice.Models;
+using Couchbase.Core;
+using Couchbase.Extensions.DependencyInjection;
+using Couchbase;
+using System.IO;
 
 namespace HelloMicroservice.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBucket _bucket;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBucketProvider bucketProvider)
         {
             _logger = logger;
+            _bucket = bucketProvider.GetBucket("hellomicroservice");
         }
 
         public IActionResult Index()
         {
-            return View();
+            var id = Guid.NewGuid().ToString();
+            _bucket.Insert(new Document<dynamic>
+            {
+                Id = id,
+                Content = new
+                {
+                    hello = "microservice",
+                    foo = Path.GetRandomFileName()
+                }
+            });
+            var doc = _bucket.Get<dynamic>(id);
+            return View(doc);
         }
 
         public IActionResult Privacy()
--- HelloMicroservice/Startup.cs
+++ HelloMicroservice/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Couchbase.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace HelloMicroservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddCouchbase(Configuration.GetSection("Couchbase"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
